4dd13afdc96cb17f4e3a1f0f4c7119ae17266ac2
Aaron Bach <bachya1208@gmail.com>
2019-10-31 21:02:19 -0600

    Add retry logic upon 401 response (#12)

--- tests/test_api.py
+++ tests/test_api.py
@@ -20,6 +20,70 @@ from .fixtures import (
 
 
 @pytest.mark.asyncio
+async def test_401_refresh_failure(
+    aresponses, devices_list_json, event_loop, login_success_json
+):
+    """Test that multiple 401 responses in a row raises the right exception."""
+    aresponses.add(
+        "mysecurity.eufylife.com",
+        "/api/v1/passport/login",
+        "post",
+        aresponses.Response(text=json.dumps(login_success_json), status=200),
+    )
+    aresponses.add(
+        "mysecurity.eufylife.com",
+        "/api/v1/app/get_devs_list",
+        "post",
+        aresponses.Response(text=None, status=401),
+    )
+    aresponses.add(
+        "mysecurity.eufylife.com",
+        "/api/v1/passport/login",
+        "post",
+        aresponses.Response(text=None, status=401),
+    )
+
+    async with aiohttp.ClientSession(loop=event_loop) as websession:
+        with pytest.raises(InvalidCredentialsError):
+            await async_login(TEST_EMAIL, TEST_PASSWORD, websession)
+
+
+@pytest.mark.asyncio
+async def test_401_refresh_success(
+    aresponses, devices_list_json, event_loop, login_success_json
+):
+    """Test that a 401 response re-authenticates successfully."""
+    aresponses.add(
+        "mysecurity.eufylife.com",
+        "/api/v1/passport/login",
+        "post",
+        aresponses.Response(text=json.dumps(login_success_json), status=200),
+    )
+    aresponses.add(
+        "mysecurity.eufylife.com",
+        "/api/v1/app/get_devs_list",
+        "post",
+        aresponses.Response(text=json.dumps(devices_list_json), status=401),
+    )
+    aresponses.add(
+        "mysecurity.eufylife.com",
+        "/api/v1/passport/login",
+        "post",
+        aresponses.Response(text=json.dumps(login_success_json), status=200),
+    )
+    aresponses.add(
+        "mysecurity.eufylife.com",
+        "/api/v1/app/get_devs_list",
+        "post",
+        aresponses.Response(text=json.dumps(devices_list_json), status=200),
+    )
+
+    async with aiohttp.ClientSession(loop=event_loop) as websession:
+        api = await async_login(TEST_EMAIL, TEST_PASSWORD, websession)
+        assert len(api.cameras) == 2
+
+
+@pytest.mark.asyncio
 async def test_bad_email(aresponses, event_loop, login_invalid_email_json):
     """Test authenticating with a bad email."""
     aresponses.add(
@@ -73,18 +137,40 @@ async def test_empty_response(
 
 
 @pytest.mark.asyncio
-async def test_http_error(aresponses, event_loop, login_success_json):
-    """Test the Eufy Security web API returning a non-2xx HTTP error code."""
+async def test_expired_access_token(
+    aresponses, devices_list_json, event_loop, login_success_json
+):
+    """Test that an expired access token refreshes automatically and correctly."""
     aresponses.add(
         "mysecurity.eufylife.com",
         "/api/v1/passport/login",
         "post",
-        aresponses.Response(text=None, status=500),
+        aresponses.Response(text=json.dumps(login_success_json), status=200),
+    )
+    aresponses.add(
+        "mysecurity.eufylife.com",
+        "/api/v1/app/get_devs_list",
+        "post",
+        aresponses.Response(text=json.dumps(devices_list_json), status=200),
+    )
+    aresponses.add(
+        "mysecurity.eufylife.com",
+        "/api/v1/passport/login",
+        "post",
+        aresponses.Response(text=json.dumps(login_success_json), status=200),
+    )
+    aresponses.add(
+        "mysecurity.eufylife.com",
+        "/api/v1/app/get_devs_list",
+        "post",
+        aresponses.Response(text=json.dumps(devices_list_json), status=200),
     )
 
     async with aiohttp.ClientSession(loop=event_loop) as websession:
-        with pytest.raises(RequestError):
-            await async_login(TEST_EMAIL, TEST_PASSWORD, websession)
+        api = await async_login(TEST_EMAIL, TEST_PASSWORD, websession)
+        api._token_expiration = datetime.now() - timedelta(seconds=10)
+        await api.async_update_device_info()
+        assert len(api.cameras) == 2
 
 
 @pytest.mark.asyncio
@@ -118,6 +204,21 @@ async def test_get_history(
 
 
 @pytest.mark.asyncio
+async def test_http_error(aresponses, event_loop, login_success_json):
+    """Test the Eufy Security web API returning a non-2xx HTTP error code."""
+    aresponses.add(
+        "mysecurity.eufylife.com",
+        "/api/v1/passport/login",
+        "post",
+        aresponses.Response(text=None, status=500),
+    )
+
+    async with aiohttp.ClientSession(loop=event_loop) as websession:
+        with pytest.raises(RequestError):
+            await async_login(TEST_EMAIL, TEST_PASSWORD, websession)
+
+
+@pytest.mark.asyncio
 async def test_login_success(
     aresponses, devices_list_json, event_loop, login_success_json
 ):
@@ -141,41 +242,4 @@ async def test_login_success(
         assert api._password == TEST_PASSWORD
         assert api._token is not None
         assert api._token_expiration is not None
-        assert len(api.cameras) == 2
-
-
-@pytest.mark.asyncio
-async def test_refreshing_access_token(
-    aresponses, devices_list_json, event_loop, login_success_json
-):
-    """Test that an expired access token refreshes automatically and correctly."""
-    aresponses.add(
-        "mysecurity.eufylife.com",
-        "/api/v1/passport/login",
-        "post",
-        aresponses.Response(text=json.dumps(login_success_json), status=200),
-    )
-    aresponses.add(
-        "mysecurity.eufylife.com",
-        "/api/v1/app/get_devs_list",
-        "post",
-        aresponses.Response(text=json.dumps(devices_list_json), status=200),
-    )
-    aresponses.add(
-        "mysecurity.eufylife.com",
-        "/api/v1/passport/login",
-        "post",
-        aresponses.Response(text=json.dumps(login_success_json), status=200),
-    )
-    aresponses.add(
-        "mysecurity.eufylife.com",
-        "/api/v1/app/get_devs_list",
-        "post",
-        aresponses.Response(text=json.dumps(devices_list_json), status=200),
-    )
-
-    async with aiohttp.ClientSession(loop=event_loop) as websession:
-        api = await async_login(TEST_EMAIL, TEST_PASSWORD, websession)
-        api._token_expiration = datetime.now() - timedelta(seconds=10)
-        await api.async_update_device_info()
         assert len(api.cameras) == 2
